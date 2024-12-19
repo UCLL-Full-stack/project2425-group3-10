047a56902ca01492540cc1689208dc99e7463a22
Jannes Vliegen <r0844937@ucll.be>
2024-12-19 10:56:43 +0100

    functioning activity fetching

--- back-end/domain/model/activity.ts
+++ back-end/domain/model/activity.ts
@@ -5,15 +5,18 @@ export class Activity {
     readonly id: number;
     readonly name: string;
     readonly type: TypeOfActivity;
+    readonly gameId: number;
 
     constructor(activity: {
         id: number;
         name: string;
-        type: TypeOfActivity
+        type: TypeOfActivity;
+        gameId: number;
     }) {
         this.id = activity.id;
         this.name = activity.name;
         this.type = activity.type;
+        this.gameId = activity.gameId;
     }
 
     getId(): number | undefined {
@@ -28,15 +31,20 @@ export class Activity {
         return this.type;
     }
 
+    getGameId(): number { // Adjusted to handle undefined
+        return this.gameId;
+    }
+
     equals(activity: Activity): boolean {
         return (
             this.id === activity.getId() &&
             this.name === activity.getName() &&
-            this.type === activity.getType()
+            this.type === activity.getType() &&
+            this.gameId === activity.getGameId() // Handles undefined
         );
     }
 
-    static from({ id, name, type }: ActivityPrisma): Activity {
-        return new Activity({id, name, type: type as TypeOfActivity });
+    static from({ id, name, type, gameId }: ActivityPrisma): Activity {
+        return new Activity({ id, name, type: type as TypeOfActivity, gameId }); // Supports undefined gameId
     }
 }
--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -2,4 +2,12 @@ type Role = 'admin' | 'user';
 type Genre = 'FPS' | 'RPG' | 'MMO' | 'INDIE'
 type TypeOfActivity = 'PVP'|'PVE'| 'C-OOP'
 
-export { Role, Genre, TypeOfActivity };
+type Activity = {
+    name: string;
+    id: number;
+    type: string;
+    gameId: number | null; // Ensure this property exists
+};
+
+export { Role, Genre, TypeOfActivity, Activity };
+
